048feb94cfc87bf03da372c06bebe20c8dc9643b
Milkenm <miguel.campos.johnny@gmail.com>
2020-05-01 22:37:25 +0100

    Update version to 1.1.0

--- ChillinRoomGMG/Properties/AssemblyInfo.cs
+++ ChillinRoomGMG/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Chillin'Room GMG")]
-[assembly: AssemblyDescription("GUI for XMRig")]
+[assembly: AssemblyDescription("XMRig with a GUI, auto-mining and other stuff")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Milkenm")]
 [assembly: AssemblyProduct("Chillin'Room GMG")]
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
 [assembly: NeutralResourcesLanguage("en")]
