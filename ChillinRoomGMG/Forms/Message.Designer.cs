660aabcb65a2bc9b83d33bc47243caa1c13be0c8
Milkenm <miguel.campos.johnny@gmail.com>
2020-05-01 22:37:21 +0100

    Message form is now visible in the task bar

--- ChillinRoomGMG/Forms/Message.Designer.cs
+++ ChillinRoomGMG/Forms/Message.Designer.cs
@@ -109,7 +109,6 @@
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "Message";
-			this.ShowInTaskbar = false;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Message";
 			this.panel_background.ResumeLayout(false);
