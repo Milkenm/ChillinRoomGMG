945850880639b00ce48e4ce8fccf1dcfd88d4c57
Milkenm <miguel.campos.johnny@gmail.com>
2021-03-31 10:08:13 +0100

    Update version

--- GMG WPF/Properties/AssemblyInfo.cs
+++ GMG WPF/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GMG WPF")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("GMG Miner")]
+[assembly: AssemblyDescription("XMRig with a GUI, auto-mining and other stuff")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GMG WPF")]
+[assembly: AssemblyCompany("Milkenm")]
+[assembly: AssemblyProduct("GMG Miner")]
 [assembly: AssemblyCopyright("Copyright © Milkenm 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -49,5 +50,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
